a468f91b2e9c06b28fd5971a5ab8c72371413cd8
Aaron Free <aaron@habitaware.com>
2021-06-12 01:59:18 -0500

    fix androidx support issue

--- android/library/src/main/java/com/polidea/multiplatformbleadapter/utils/mapper/RxBleDeviceToDeviceMapper.java
+++ android/library/src/main/java/com/polidea/multiplatformbleadapter/utils/mapper/RxBleDeviceToDeviceMapper.java
@@ -1,7 +1,7 @@
 package com.polidea.multiplatformbleadapter.utils.mapper;
 
-import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 import com.polidea.multiplatformbleadapter.Device;
 import com.polidea.rxandroidble2.RxBleConnection;
